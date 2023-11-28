78b5fb2a90e1017bcc4d16a30521526149d47ddd
CaseDuncan <oluochdancun19@gmail.com>
2023-11-28 20:52:47 +0300

    implemented django signals feature to generate code on creation

--- antique/apps.py
+++ antique/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class AntiqueConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'antique'
+
+    def ready(self):
+        import antique.signals
+        
