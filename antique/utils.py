56eb247a5b286ffc74d160a305b3a0a27495641e
CaseDuncan <oluochdancun19@gmail.com>
2023-11-29 09:14:10 +0300

    implement SMS auth with twilio

---
+++ antique/utils.py
@@ -0,0 +1,9 @@
+import os
+# from twilio.rest import Client
+
+account_sid =''
+auth_token = ''
+# client = Client(account_sid, auth_token)
+def send_SMS(verification_code, phone_number):
+    # message = Client.messages.create(body=f"Hi, your verification code to LovejoyAntique app is {verification_code}", from='', to=f" {phone_number} ")
+    pass
--- antique/views.py
+++ antique/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, HttpResponseRedirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import authenticate, login, logout
+from antique.forms import VerificationCodeForm
+from antique.models import CustomUser
+from .utils import send_SMS
 # Create your views here.
+@login_required
+def submit_evaluation_view(request):
+    return render(request, 'user/create_antique_eval.html')
+
+def auth_view(request):
+    form = AuthenticationForm()
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(request,username=username, password=password)
+        if user is not None:
+            request.session['pk'] = user.pk
+            return HttpResponseRedirect('/verify/')
+    return render(request, 'user/login.html', {'form':form})
+
+def verification_view(request):
+    form = VerificationCodeForm(request.POST or None)
+    pk = request.session.get('pk')
+    if pk:
+        user = CustomUser.objects.get(pk=pk)
+        code = user.code
+        code_user = f"{user.username}: {code}"
+
+        if not request.POST:
+            #send SMS
+            print(code_user)
+            send_SMS(code_user, user.phone_number)
+        if form.is_valid():
+                verification_code = form.cleaned_data.get('code')
+                if str(code) == verification_code:
+                    code.save()
+                    login(request, user)
+                    return HttpResponseRedirect('/submit_evaluation/')
+                else:
+                    return HttpResponseRedirect('/login/')    
+    return render(request, 'user/verify.html', {'form': form})
+
